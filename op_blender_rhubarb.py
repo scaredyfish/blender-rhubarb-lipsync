a0a2314dfc913b561fb982e6585c4adf4296adde
Andrew Charlton <adcharlton@gmail.com>
2018-05-05 16:05:04 +1200

    Select files and poses from panel

--- op_blender_rhubarb.py
+++ op_blender_rhubarb.py
@@ -7,6 +7,7 @@ from threading  import Thread
 from queue import Queue, Empty
 import json
 
+
 class RhubarbLipsyncOperator(bpy.types.Operator):
     """Move an object with the mouse, example"""
     bl_idname = "object.rhubarb_lipsync"
@@ -23,7 +24,6 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
                 wm.event_timer_remove(self._timer)
 
                 results = json.loads(stdout)
-              #  print(results)
                 fps = context.scene.render.fps
                 lib = context.object.pose_library
 
@@ -31,9 +31,9 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
 
                 for cue in results['mouthCues']:
                     print("start: {0} frame: {1} value: {2}".format(cue['start'], cue['start'] * fps , cue['value']))
-                    pose_name = self.cue_prefix + cue['value']
-                    #pose_index = lib.pose_markers[pose_name].frame -1 #
-                    pose_index = lib.pose_markers.keys().index(pose_name)
+
+                    pose_index = context.object.pose_library.mouth_shapes['mouth_' + cue['value'].lower()]
+
                     frame_num = round(cue['start'] * fps)
 
                     context.scene.frame_set(frame_num)
@@ -46,8 +46,6 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
 
                 return {'FINISHED'}
         except subprocess.TimeoutExpired as ex:
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            print(template.format(type(ex).__name__, ex.args))
             return {'PASS_THROUGH'}
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
@@ -55,18 +53,14 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
             return {'CANCELLED'}
 
 
-
-
-
     def invoke(self, context, event):
-      #  inputfile = "D:/Cloud/OneDrive/rhubarb/67703__acclivity__alphabet-male.wav"
-        inputfile = "D:/Cloud/OneDrive/rhubarb/8323__levinj__powerwords-english.wav"
-        dialogfile = "D:/Cloud/OneDrive/rhubarb/powerwords.txt"
+        inputfile = context.object.pose_library.mouth_shapes.sound_file
+        dialogfile = context.object.pose_library.mouth_shapes.dialog_file
         self.rhubarb = subprocess.Popen("D:/Cloud/OneDrive/rhubarb/rhubarb.exe -f json --machineReadable --dialogFile %s --extendedShapes GHX %s" % (dialogfile, inputfile),
                                    stdout=subprocess.PIPE, universal_newlines=True)
 
         wm = context.window_manager
-        self._timer = wm.event_timer_add(0.1, context.window)
+        self._timer = wm.event_timer_add(1, context.window)
 
         wm.modal_handler_add(self)
 
--- pnl_blender_rhubarb.py
+++ pnl_blender_rhubarb.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy_extras.io_utils import ImportHelper
 from . import  op_blender_rhubarb
 
 class RhubarbLipsyncPanel(bpy.types.Panel):
@@ -17,19 +18,34 @@ class RhubarbLipsyncPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
+        prop = context.object.pose_library.mouth_shapes
+
         col = layout.column()
-        col.prop(context.object.pose_library, 'mouth_a', "Mouth A")
-        col.prop(context.object.pose_library, 'mouth_b', "Mouth B")
-        col.prop(context.object.pose_library, 'mouth_c', "Mouth C")
-        col.prop(context.object.pose_library, 'mouth_d', "Mouth D")
-        col.prop(context.object.pose_library, 'mouth_e', "Mouth E")
-        col.prop(context.object.pose_library, 'mouth_f', "Mouth F")
-        col.prop(context.object.pose_library, 'mouth_g', "Mouth G")
-        col.prop(context.object.pose_library, 'mouth_h', "Mouth H")
-        col.prop(context.object.pose_library, 'mouth_x', "Mouth X")
+        col.prop(prop, 'mouth_a', "Mouth A")
+        col.prop(prop, 'mouth_b', "Mouth B")
+        col.prop(prop, 'mouth_c', "Mouth C")
+        col.prop(prop, 'mouth_d', "Mouth D")
+        col.prop(prop, 'mouth_e', "Mouth E")
+        col.prop(prop, 'mouth_f', "Mouth F")
+        col.prop(prop, 'mouth_g', "Mouth G")
+        col.prop(prop, 'mouth_h', "Mouth H")
+        col.prop(prop, 'mouth_x', "Mouth X")
+
+        row = layout.row(align=True)
+        row.prop(prop, 'sound_file', text='Sound file')
+        row.operator("object.sound_file_selector", icon="FILE_FOLDER", text="")
+
+        row = layout.row(align=True)
+        row.prop(prop, 'dialog_file', text='Dialog file')
+        row.operator("object.dialog_file_selector", icon="FILE_FOLDER", text="")
+
+        row = layout.row()
+        row.prop(prop, 'start_frame', text='Start frame')
 
         row = layout.row()
         row.operator(operator = "object.rhubarb_lipsync")
+
+
 
 pose_markers = []
 
@@ -42,28 +58,59 @@ def pose_markers_items(self, context):
         return []
 
     pose_markers = [(marker, marker, 'Poses', '', idx) for idx, marker in enumerate(lib.pose_markers.keys())]
-    print(pose_markers)
     return pose_markers
 
+poses = bpy.props.EnumProperty(
+    items=pose_markers_items,
+    name='Poses',
+    description='Poses',
+)
+
+class MouthShapesProperty(bpy.types.PropertyGroup):
+    mouth_a = poses
+    mouth_b = poses
+    mouth_c = poses
+    mouth_d = poses
+    mouth_e = poses
+    mouth_f = poses
+    mouth_g = poses
+    mouth_h = poses
+    mouth_x = poses
+
+    sound_file = bpy.props.StringProperty(name="sound_file")
+    dialog_file = bpy.props.StringProperty(name="dialog_file")
+
+    start_frame = bpy.props.IntProperty(name="start_frame")
+
+class SoundFileSelector(bpy.types.Operator, ImportHelper):
+    bl_idname = "object.sound_file_selector"
+    bl_label = "some folder"
+
+    filename_ext = ""
+
+    def execute(self, context):
+        fdir = self.properties.filepath
+        context.object.pose_library.mouth_shapes.sound_file = fdir
+        return{'FINISHED'}
+
+class DialogFileSelector(bpy.types.Operator, ImportHelper):
+    bl_idname = "object.dialog_file_selector"
+    bl_label = "some folder"
+
+    filename_ext = "txt"
+
+    def execute(self, context):
+        fdir = self.properties.filepath
+        context.object.pose_library.mouth_shapes.dialog_file = fdir
+        return {'FINISHED'}
+
 def register():
+    bpy.utils.register_class(SoundFileSelector)
+    bpy.utils.register_class(DialogFileSelector)
+    bpy.utils.register_class(MouthShapesProperty)
     bpy.utils.register_class(RhubarbLipsyncPanel)
 
-    poses = bpy.props.EnumProperty(
-        items=pose_markers_items,
-        name='Poses',
-        description='Poses',
-    )
-
-    bpy.types.Action.mouth_a = poses
-    bpy.types.Action.mouth_b = poses
-    bpy.types.Action.mouth_c = poses
-    bpy.types.Action.mouth_d = poses
-    bpy.types.Action.mouth_e = poses
-    bpy.types.Action.mouth_f = poses
-    bpy.types.Action.mouth_g = poses
-    bpy.types.Action.mouth_h = poses
-    bpy.types.Action.mouth_x = poses
-
+    bpy.types.Action.mouth_shapes = bpy.props.PointerProperty(type=MouthShapesProperty)
 
 
 def unregister():
