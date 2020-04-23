55ef349926528ff70f52939a549990fe3e202796
Andrew Charlton <adcharlton@gmail.com>
2020-04-23 15:42:59 +1200

    automatically extract version from init.py

--- package.sh
+++ package.sh
@@ -2,13 +2,15 @@ cp *.py dist/linux
 cp *.py dist/windows
 cp *.py dist/osx
 
+VERSION="$(grep -Po "__version__ = '\K[0-9\.]*"  __init__.py)"
+
 cd dist/linux
-zip -r ../blender-rhubarb-lipsync-linux-3.0.0.zip *
+zip -r "../blender-rhubarb-lipsync-linux-$VERSION.zip" *
 
 cd ../windows
-zip -r ../blender-rhubarb-lipsync-windows-3.0.0.zip *
+zip -r "../blender-rhubarb-lipsync-windows-$VERSION.zip" *
 
 cd ../osx
-zip -r ../blender-rhubarb-lipsync-osx-3.0.0.zip *
+zip -r "../blender-rhubarb-lipsync-osx-$VERSION.zip" *
 
 cd ../..
