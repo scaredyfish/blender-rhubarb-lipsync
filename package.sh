ff51f2f29226e8ed09231585a656772d82dd03f2
Andrew Charlton <adcharlton@gmail.com>
2020-04-27 14:06:50 +1200

    Fix packaging script to include top level directory

--- package.sh
+++ package.sh
@@ -1,16 +1,18 @@
-cp *.py dist/linux
-cp *.py dist/windows
-cp *.py dist/osx
+cp *.py dist/linux/blender-rhubarb-lipsync
+cp *.py dist/windows/blender-rhubarb-lipsync
+cp *.py dist/osx/blender-rhubarb-lipsync
 
 VERSION="$(grep -Po "__version__ = '\K[0-9\.]*"  __init__.py)"
 
-cd dist/linux
-zip -r "../blender-rhubarb-lipsync-linux-$VERSION.zip" *
+cd dist
+
+cd linux
+zip -r "../blender-rhubarb-lipsync-linux-$VERSION.zip" blender-rhubarb-lipsync
 
 cd ../windows
-zip -r "../blender-rhubarb-lipsync-windows-$VERSION.zip" *
+zip -r "../blender-rhubarb-lipsync-windows-$VERSION.zip" blender-rhubarb-lipsync
 
 cd ../osx
-zip -r "../blender-rhubarb-lipsync-osx-$VERSION.zip" *
+zip -r "../blender-rhubarb-lipsync-osx-$VERSION.zip" blender-rhubarb-lipsync
 
-cd ../..
+cd ..
