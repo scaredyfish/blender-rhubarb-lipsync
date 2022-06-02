8feeec120dfec73fb9d06ece8a1c32ee65390e9c
NickTiny <nick@tinymedia.ca>
2022-06-01 21:40:49 -0400

    Update Blender Version to 3.0+

--- __init__.py
+++ __init__.py
@@ -4,14 +4,14 @@ bl_info = {
     "name": "Rhubarb Lipsync-DEV",
     "author": "Customized by Nick Alberelli, based on: Addon by Andrew Charlton, includes Rhubarb Lip Sync by Daniel S. Wolf",
     "version": (3, 0, 2),
-    "blender": (2, 83, 0),
+    "blender": (3, 0, 0),
     "location": "VIEW3D > Sidebar > AI Lipsync",
     "description": "Integrate Rhubarb Lipsync into Tiny Media Blender Workflow",
     "wiki_url": "https://github.com/adcharlton/blender-rhubarb-lipsync",
     "tracker_url": "https://github.com/adcharlton/blender-rhubarb-lipsync/issues",
     "support": "COMMUNITY",
     "category": "Animation",
-    "warning": "This addon is not ready to be used by anyone.",
+    "warning": "This addon is in development and NOT ready to for production.",
 }
 
 
